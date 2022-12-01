347d5f07d26dccf9ea84abb6f6be4db2e0cabaf2
Joacim Bohlander <joacimbohlander@gmail.com>
2022-12-01 22:16:13 +0100

    new

--- lineCounter/lineCounter/Program.cs
+++ lineCounter/lineCounter/Program.cs
@@ -1,20 +1,31 @@
 ﻿using System.IO;
+using System;
+using System.Collections.Generic;
 
 namespace lineCounter
 {
     class Program
     {
+        List<int> ints;
         static void Main(string[] args)
         {
+            List<int> linesArr = new List<int>();
+
             // This opens the file at path and counts the line
             var lineCount = 0;
+            string line;
             path = @"";
-            using (StreamReader sr = new StreamReader(path))
+            using StreamReader sr = new StreamReader(path);
+
+            while  (sr.ReadLine() != null) 
             {
-                while (sr.ReadLine() != null)
+                for (int i = 0; i < linesArr.Count; i++)
                 {
-                    lineCount++;
+                    int line = linesArr[i];
                 }
+
+
+                lineCount++;
             }
             // sum = lineCount(lineCount+1)/2
 
