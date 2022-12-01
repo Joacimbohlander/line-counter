b8d7e89b0cb5f4114617cf102bbf472ea7ce38bc
Joacim Bohlander <joacimbohlander@gmail.com>
2022-12-01 22:11:43 +0100

    added new boiler

---
+++ lineCounter/lineCounter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+
+namespace lineCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This opens the file at path and counts the line
+            var lineCount = 0;
+            path = @"";
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while (sr.ReadLine() != null)
+                {
+                    lineCount++;
+                }
+            }
+            // sum = lineCount(lineCount+1)/2
+
+        }
+    }
+}
